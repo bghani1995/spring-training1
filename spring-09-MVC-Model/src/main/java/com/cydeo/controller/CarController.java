76f04345da232cdc5fe21a13a67df81ab8fcf777
Bilal Ghani <bghani1995@gmail.com>
2022-09-23 19:51:33 -0500

    Spring Boot

---
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -0,0 +1,13 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CarController {
+
+    public String carInfo(){
+
+
+        return "car/car-info";
+    }
+}
