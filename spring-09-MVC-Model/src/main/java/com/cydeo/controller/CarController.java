6163ea5a145a3d5586f0ff4c9ce2db43e5d7144c
Bilal Ghani <bghani1995@gmail.com>
2022-09-23 20:03:46 -0500

    Spring Boot

--- spring-09-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class CarController {
 
+    //localhost:8080/info?make=Honda
     @RequestMapping("/info")
     public String carInfo(@RequestParam String make, Model model){
 
@@ -15,11 +16,22 @@ public class CarController {
 
         return "car/car-info";
     }
-//Default
+
+    //localhost:8080/info2 (KIA)
     @RequestMapping("/info2")
     public String carInfo2(@RequestParam(value = "make", required = false,defaultValue = "KIA") String make, Model model){
 
         model.addAttribute("make", make);
+
+        return "car/car-info";
+    }
+
+    //localhost:8080/info3?make=Honda&year=2015
+    @RequestMapping("/info3")
+    public String carInfo3(@RequestParam String make, @RequestParam int year, Model model){
+
+        model.addAttribute("make", make);
+        model.addAttribute("year", year);
 
         return "car/car-info";
     }
