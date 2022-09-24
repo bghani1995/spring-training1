08acf5cc3234f78a45b9240ee342f7f6b00b7861
Bilal Ghani <bghani1995@gmail.com>
2022-09-23 19:02:06 -0500

    Spring Boot

--- spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Random;
+
 @Controller
 public class StudentController {
 
@@ -12,6 +14,12 @@ public class StudentController {
 
         model.addAttribute("name", "Cydeo");
         model.addAttribute("course", "MVC");
+
+        String subject = "Spring Boot";
+        model.addAttribute("subject", subject);
+
+        int studentId = new Random().nextInt();
+        model.addAttribute("id", studentId);
 
          return "student/welcome";
     }
