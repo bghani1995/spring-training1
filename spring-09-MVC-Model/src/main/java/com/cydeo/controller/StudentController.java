5b170ad09c4098a59ba5ad0f5d009b834985b0c8
Bilal Ghani <bghani1995@gmail.com>
2022-09-23 19:06:19 -0500

    Spring Boot

--- spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @Controller
@@ -20,6 +23,17 @@ public class StudentController {
 
         int studentId = new Random().nextInt();
         model.addAttribute("id", studentId);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+
+        model.addAttribute("numbers", numbers);
+
+        LocalDate dt = LocalDate.now();
+        model.addAttribute("date", dt);
 
          return "student/welcome";
     }
