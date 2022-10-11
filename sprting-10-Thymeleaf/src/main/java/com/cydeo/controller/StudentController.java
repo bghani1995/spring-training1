d9bd4f4f19ad88be8e290d18949dd61fe1cbce5f
Bilal Ghani <bghani1995@gmail.com>
2022-10-11 11:33:07 -0500

    Thymeleaf

--- sprting-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ sprting-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,6 +3,8 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register")//localhost:8080/student/register
+   // @RequestMapping("/register", method = RequestMethod.GET)//localhost:8080/student/register
+    @GetMapping("/register")
     public String register(Model model){
 
         model.addAttribute("students", DataGenerator.createStudent());
@@ -18,7 +21,8 @@ public class StudentController {
         return "student/register";
     }
 
-    @RequestMapping("/welcome")//localhost:8080/student/register
+    //@RequestMapping("/welcome")//localhost:8080/student/register
+    @PostMapping("/welcome")
     public String welcome(){
 
 
